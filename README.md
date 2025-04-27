98e361ae66ba14bee788db497775c4d46afaaf5e
Alejandro Copado <alexcopado2005@gmail.com>
2025-04-27 13:53:38 +0200

    Realizacion de rutas.

--- README.md
+++ README.md
@@ -19,5 +19,19 @@ Proyecto creado con Vite
 npm create vite@latest patrigod_front
 ```
 
+He dejado el proyecto desde 0 y ya he empezado a programar.
+Carpetas:
+- componentes
+- contexto
+- paginas
+- servicios
+Primero voy a crear toda las carpetas de mi proyecto y luego creare las rutas y todo en general y a partir de ahi, poco a poco pues le dare caña.
+
+```bash
+npm i react-router-dom
+```
+Y en el App.jsx voy a programar todas las rutas.
+
+
 ## Autor 
 Realizado por Alejandro Copado López
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,67 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Layout from './componentes/Layout/Layout'
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+        element:<Layout/>,
+        children:[
+          {
+            index:true,
+            element:<p>INICIO</p>,
+          },
+          {
+            path:"ciudad",
+            element:<p>Ciudad</p>,
+          },
+          {
+            path:"ciudad/numero/comidas",
+            element:<p>Comidas</p>,
+          },
+          {
+            path:"ciudad/numero/comidas/numero",
+            element:<p>Comida</p>,
+          },
+          {
+            path:"ciudad/numero/monumentos",
+            element:<p>Monumentos</p>,
+          },
+          {
+            path:"ciudad/numero/monumentos/numero",
+            element:<p>Monumento</p>,
+          },
+          {
+            path:"ciudad/numero/eventos",
+            element:<p>Eventos</p>,
+          },
+          {
+            path:"ciudad/numero/eventos/numero",
+            element:<p>Comida</p>,
+          },
+          {
+            path:"acerda",
+            element:<p>Informacion</p>,
+          },
+          {
+            path:"datos_usuario",
+            element:<p>Informacion Usuario</p>,
+          },
+          {
+            path:"/login",
+            element:<p>Login</p>,
+          },
+          {
+            path:"/logout",
+            element:<p>Logout</p>,
+          }
+        ]
+  }
+])
 function App() {
   return (
     <>
+      <RouterProvider router={router}/>
     </>
   )
 }
